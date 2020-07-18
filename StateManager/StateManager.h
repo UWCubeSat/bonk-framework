91462b5de4f6f69d8e47874e626221273e0218be
Eli Reed <sailedeer11@gmail.com>
2020-07-17 22:42:02 -0700

    Added functions to interface, removed some others

--- StateManager/StateManager.h
+++ StateManager/StateManager.h
@@ -1,19 +1,28 @@
 #ifndef STATE_MANAGER_H_
 #define STATE_MANAGER_H_
 
+#include <stdint.h>   // for uint64_t, uint32_t
+
 #include "Arduino.h"
 
+template <class S>
 class StateManager
 {
   public:
     StateManager();
-    void* getState() const;
-    bool setState(void* new_state_st);
-    bool flushToSD() const;
+    bool read_state(S& state);
+    S& get_state() { return _state };
+    bool write_state(const S& state);
+    bool set_state(const S& state);
+    bool flush_to_sd() const;
+
   private:
-    uint32_t _writes;
-    void* _state_st;
-    bool _last_successful;    
+    unsigned long crc(void) const;
+    bool filled() const;
+
+    uint64_t _write_size;
+    S _state;
+    S _last_state;
 };  // StateManager class
 
 #endif  // STATE_MANAGER_H_
