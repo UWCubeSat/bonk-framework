116eb2fe176ae90a0d0cab7f226fccd8e1dc4aee
Eli Reed <sailedeer11@gmail.com>
2020-07-19 15:50:39 -0700

    Changed CRC and some method signatures

--- StateManager/src/StateManager.h
+++ StateManager/src/StateManager.h
@@ -10,70 +10,69 @@ template <typename S>
 class StateManager
 {
   public:
-    StateManager(S &default_state, const char *filepath);
+    StateManager(const S& default_state, const char *filepath);
     S get_state() const { return _state; }
-    bool set_state(const S &state);
+    bool set_state(const S& state);
+    uint16_t get_write_count() const { return _write_count; }
     bool flush_to_sd();
 
   private:
-    unsigned long crc(void) const;
+    uint32_t crc32(const S& state) const;
     bool filled() const;
-    bool write_state(const S &state);
-    bool read_state(S& state);
+    bool write_state(const S& state);
+    bool read_state(S& state) const;
 
-    uint64_t _write_size;
-    uint32_t _write_count;
+    uint16_t _write_count;
+    const uint16_t _write_size;
+    const uint16_t _offset;
     const char* _state_file_path;
-    const uint8_t _offset;
-    const uint16_t _magic_number;
-    const uint8_t _data_start;
-    S _default_state;
+    const S _default_state;
     S _state;
 };  // StateManager class
 
 template <typename S>
-StateManager<S>::StateManager(S &default_state, const char *filepath) : 
+StateManager<S>::StateManager(const S& default_state, const char *filepath) :
+                              _write_size(sizeof(S)),
                               _offset(sizeof(uint32_t) + sizeof(uint16_t)),
-                              _magic_number(0x451b),
-                              _data_start(_offset + sizeof(uint32_t)) {
-    _default_state = default_state;
-    _state_file_path = filepath;
-    _write_size = sizeof(S);
+                              _state_file_path(filepath),
+                              _default_state(default_state) {
 
+    // uint32_t crc;
+    // EEPROM.get(0, crc);
+    // uint32_t real_crc = StateManager::crc();
+
+    // uint16_t magic_num;
+    // EEPROM.get(sizeof(uint32_t), magic_num);
+
+    // if (crc == real_crc && magic_num == MAGIC_NUMBER) {
+    //     // the data in the EEPROM is good
+    //     // load the last state we saved
+    //     EEPROM.get(_offset, _write_count);
+    //     StateManager<S>::read_state(_state);
+    // } else {
+    //     // the data in the EEPROM are bad
+    //     // fall back to the default state
+    //     StateManager<S>::set_state(_default_state);
+    //     EEPROM.put(sizeof(uint32_t), _magic_number);
+    //     EEPROM.put(0, StateManager::crc());
+    // }
+}
+
+template <typename S>
+bool StateManager<S>::read_state(S& state) const {
+    S& ret_state = EEPROM.get(_offset + _write_size * (_write_count - 1), state);
     uint32_t crc;
-    EEPROM.get(0, crc);
-    uint32_t real_crc = StateManager::crc();
-
-    uint16_t magic_num;
-    EEPROM.get(sizeof(uint32_t), magic_num);
-
-    if (crc == real_crc && magic_num == _magic_number) {
-        // the data in the EEPROM is good
-        // load the last state we saved
-        EEPROM.get(_offset, _write_count);
-        StateManager<S>::read_state(_state);
-    } else {
-        // the data in the EEPROM are bad
-        // fall back to the default state
-        StateManager<S>::set_state(_default_state);
-        EEPROM.put(sizeof(uint32_t), _magic_number);
-        EEPROM.put(0, StateManager::crc());
-    }
+    uint32_t& ret_crc = EEPROM.get(0, crc);
+    return ret_crc == crc && StateManager::crc32(state) == crc && ret_state == state;
 }
 
 template <typename S>
-bool StateManager<S>::read_state(S &state) {
-    S& ret = EEPROM.get(_data_start + _write_size * (_write_count - 1), state);
-    return true;
-}
-
-template <typename S>
-bool StateManager<S>::write_state(const S &state) {
-    const S& ret = EEPROM.put(_data_start + _write_size * _write_count, state);
-    _write_count++;
-    EEPROM.put(_offset, _write_count);
-    EEPROM.put(0, StateManager<S>::crc());
-    return true;
+bool StateManager<S>::write_state(const S& state) {
+    uint32_t crc = StateManager::crc32(state);
+    const S& ret_state = EEPROM.put(_offset + _write_size * (_write_count++), state);
+    const uint16_t& ret_writes = EEPROM.put(sizeof(uint32_t), _write_count);
+    const uint32_t& ret_crc = EEPROM.put(0, crc);
+    return ret_state == state && ret_writes == _write_count && ret_crc == crc;
 }
 
 template <typename S>
@@ -83,8 +82,8 @@ bool StateManager<S>::set_state(const S &state) {
             return false;
         }
     }
-
-    if (StateManager<S>::write_state(state)) {
+    
+    if (StateManager::write_state(state)) {
         _state = state;
         return true;
     }
@@ -94,23 +93,19 @@ bool StateManager<S>::set_state(const S &state) {
 template <typename S>
 bool StateManager<S>::flush_to_sd() {
     File sf = SD.open(_state_file_path, O_APPEND | O_WRITE);
-    char buf[EEPROM.length() - _offset];
-    for (uint32_t i = _offset; i < EEPROM.length(); i++) {
-        buf[i - _offset] = EEPROM[i];
+    for (uint16_t i = sizeof(uint16_t); i < EEPROM.length(); i++) {
+        sf.write(EEPROM[i]);
     }
-    sf.write(buf, EEPROM.length() - _offset);
     sf.close();
 
     _write_count = 0;
-    EEPROM.put(_offset, _write_count);
-    EEPROM.put(0, StateManager::crc());
-    return true;
+    const uint16_t& ret_writes = EEPROM.put(_offset, _write_count);
+    return ret_writes == _write_count;
 }
 
-// code taken from https://www.arduino.cc/en/Tutorial/EEPROMCrc
+// code adapted from https://www.arduino.cc/en/Tutorial/EEPROMCrc
 template <typename S>
-uint32_t StateManager<S>::crc(void) const {
-
+uint32_t StateManager<S>::crc32(const S& state) const {
     constexpr uint32_t crc_table[16] = {
         0x00000000, 0x1db71064, 0x3b6e20c8, 0x26d930ac,
         0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
@@ -120,10 +115,12 @@ uint32_t StateManager<S>::crc(void) const {
 
     uint32_t crc = ~0L;
 
-    // we only want to fold bytes that AREN'T are the CRC segment/magic number
-    for (int index = _offset; index < EEPROM.length(); ++index) {
-        crc = crc_table[(crc ^ EEPROM[index]) & 0x0f] ^ (crc >> 4);
-        crc = crc_table[(crc ^ (EEPROM[index] >> 4)) & 0x0f] ^ (crc >> 4);
+    uint16_t size = sizeof(S);
+    uint8_t* data = &state;
+
+    for (uint16_t index = 0 ; index < size; ++index) {
+        crc = crc_table[(crc ^ (*(data + index))) & 0x0f] ^ (crc >> 4);
+        crc = crc_table[(crc ^ (*(data + index)) >> 4) & 0x0f] ^ (crc >> 4);
         crc = ~crc;
     }
     return crc;
@@ -131,7 +128,7 @@ uint32_t StateManager<S>::crc(void) const {
 
 template <typename S>
 bool StateManager<S>::filled() const {
-    return (_write_size * (_write_count + 1) + _data_start) > EEPROM.length();
+    return (_write_size * (_write_count + 1) + _offset) >= EEPROM.length();
 }
 
 #endif  // STATE_MANAGER_H_
