bc50eb87350d4f49dcfd44b91c23d3fa4b3294ca
Eli Reed <erobreed@uw.edu>
2020-08-15 02:38:18 -0700

    added some method headers

--- LogManager/src/LogManager.h
+++ LogManager/src/LogManager.h
@@ -3,5 +3,20 @@
 
 #include <SD.h>         // for access to SD card attached to Arduino
 
+namespace BONK {
+  enum LOG_LEVEL{DEBUG, WARNING, ERROR, NOTIFY};
+  class LogManager {
+    public:
+      LogManager(const char* log_path);
+      get_log_level() { return log_level_;}
+      set_log_level(const BONK::LOG_LEVEL level) {log_level_ = level}
+      log(const char* message);      
+    private:
+      LOG_LEVEL log_level_;
+      const char* log_path;
+
+  };  // class LogManager
+  extern LogManager Logger;
+}   // BONK namespace
 
 #endif  // LOG_MANAGER_H_
