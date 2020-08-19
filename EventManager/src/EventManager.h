e8378bc2775d2e93292a4365b49b905a45ea82f2
Eli Reed <sailedeer11@gmail.com>
2020-08-18 22:57:48 -0700

    started filling out interface

--- EventManager/src/EventManager.h
+++ EventManager/src/EventManager.h
@@ -2,10 +2,36 @@
 #ifndef EVENT_MANAGER_H_
 #define EVENT_MANAGER_H_
 
-class EventManager {
-  public: 
-  private:
-};  // class LogManager
-extern EventManager EM;
+#include <map>
+#include <vector>
+
+namespace BONK {
+  // Taken from NanoRacks Feather Frame PUG
+  enum FLIGHT_EVENT{
+    NONE = 0x40,          // @
+    ESCAPE_ENABLED,       // A
+    ESCAPE_COMMANDED,     // B
+    LIFTOFF,              // C
+    MAIN_ENGINE_CUTOFF,   // D
+    SEPARATION_COMMANDED, // E
+    COAST_START,          // F
+    APOGEE,               // G
+    COAST_END,            // H
+    DROGUE_CHUTES,        // I
+    MAIN_CHUTES,          // J
+    TOUCHDOWN,            // K
+    SAFING,               // L
+    MISSION_END,          // M
+  };
+
+  class EventManager {
+    public:
+      EventManager();
+      bool register_event(const BONK::FLIGHT_EVENT fe, bool (*function)());
+    private:
+      std::map<BONK::FLIGHT_EVENT, std::vector<bool (*)()>> cb_func_map_;
+  };  // class LogManager
+  extern EventManager Event;
+} // namespace BONK
 
 #endif  // LOG_MANAGER_H_
